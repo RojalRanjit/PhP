a45a41399e6ab2af5f030537c98f8a3c1bc087b2
RojalRanjit <90741178+RojalRanjit@users.noreply.github.com>
2022-08-09 22:33:26 +0545

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> 1st program </title>
+</head>
+<body>
+	<?php
+		define(NAME, Rojal);
+		echo "Hello World";
+		echo "I am",NAME;
+	?>
+</body>
+</html>
